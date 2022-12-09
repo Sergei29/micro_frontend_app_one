3dd8da0dc791bed42ea3177a8cdbb39b36d0590c
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-09 14:58:04 +0200

    fix: fallback to init settings

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type Props = {};
+
+const Header = ({}: Props): JSX.Element => {
+  return (
+    <div
+      style={{
+        background: "black",
+        width: "100%",
+        height: "100px",
+        color: "white",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "left",
+        alignItems: "center",
+        fontSize: "24px",
+        paddingLeft: "15px",
+      }}
+    >
+      Name
+    </div>
+  );
+};
+
+export default Header;
