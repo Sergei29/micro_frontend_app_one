db01cb532f5a0f80c3f98492fa2ab7c2c6587665
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-09 15:44:01 +0200

    feat: config: exposed nav component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "frontend_one",
+        remotes: {
+          frontend_three: `frontend_three@http://localhost:3002/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./nav": "./src/components/Nav/index.tsx",
+        },
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
